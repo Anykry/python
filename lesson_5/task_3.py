8b036736aa2c3f7480bcdbef7a6d99bb2664cc1a
Andrew <info@gavah.com.ua>
2021-07-30 06:33:22 +0300

    Task 3

---
+++ lesson_5/task_3.py
@@ -0,0 +1,20 @@
+def employee_data(line):
+    line = line.replace('\n', '')
+    str_data = line.partition(' ')
+    return str_data[0], str_data[2]
+
+
+salary_file = open('salary.txt', 'r', encoding='utf-8')
+
+sum_salary = 0
+
+lines_array = salary_file.readlines()
+for employee in lines_array:
+    last_name, salary = employee_data(employee)
+    if float(salary) < 20000:
+        print(last_name, salary)
+    sum_salary += float(salary)
+
+print(f'The average salary from file is: {sum_salary / len(lines_array)}')
+
+salary_file.close()
